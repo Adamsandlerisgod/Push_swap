4af9ca646d4ab8931d8e640bd701a39123edcc77
Wolf Hendrikse <whendrik@z2t3c3.42bangkok.com>
2023-07-05 17:10:02 +0700

    First Commit

---
+++ includes/push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: whendrik <whendrik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/08 16:08:04 by whendrik          #+#    #+#             */
+/*   Updated: 2023/07/05 16:48:21 by whendrik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "../libft/includes/libft.h"
+# include <math.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+
+#endif
